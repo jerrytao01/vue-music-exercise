9e5caf20e53239dd8677e6ed182fd1077c583fc2
jerrytao01 <841869544@qq.com>
2019-02-06 14:10:40 +0800

    播放vuex设计

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,13 @@
+import {playMode} from '../common/js/config'
+
 const state = {
-  singer: {} // 歌手
+  singer: {}, // 歌手,
+  playing: false, // 播放
+  fullScreen: false, // 全屏
+  playlist: [], // 播放列表
+  sequenceList: [], // 随机播放列表
+  mode: playMode.sequence, // 播放模式
+  currentIndex: -1
 }
 
 export default state
