29aac582ad2a0d60215a4f5b2f730525cdbb3c9f
jerrytao01 <841869544@qq.com>
2019-02-13 23:57:48 +0800

    歌词完成

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -2,54 +2,68 @@
   <div class="player" v-show="playList.length>0">
     <transition name="normal">
       <div class="normal-player" v-show="fullScreen">
-      <div class="background">
-        <div class="mask"></div>
-        <img width="100%" height="100%" :src="currentSong.image">
-      </div>
-      <div class="top">
-        <span class="iconfont icon-double-underarrow under-arrow" @click="back"></span>
-        <h2 class="top-title">{{currentSong.name}}</h2>
-      </div>
-      <p class="singer">{{currentSong.singer}}</p>
-      <div class="album-wrapper">
-        <div class="img-wrapper" :class="cdCls">
-          <div class="cd-img-wrapper">
-            <img class="cd-img" :src="currentSong.image" alt="">
+        <div class="background">
+          <div class="mask"></div>
+          <img width="100%" height="100%" :src="currentSong.image">
+        </div>
+        <div class="top">
+          <span class="iconfont icon-double-underarrow under-arrow" @click="back"></span>
+          <h2 class="top-title">{{currentSong.name}}</h2>
+        </div>
+        <p class="singer">{{currentSong.singer}}</p>
+        <transition name="cd-fade">
+          <div class="album-wrapper" v-show="toggleLyric">
+            <div class="img-wrapper" :class="cdCls">
+              <div class="cd-img-wrapper">
+                <img class="cd-img" :src="currentSong.image" alt="">
+              </div>
+            </div>
+          </div>
+        </transition>
+        <transition name="fade">
+          <scroll ref="lyricList" v-if="!toggleLyric" :data="currentLyric && currentLyric.lines" class="scroll-lyric">
+            <div class="lyric-wrapper" @click.stop="_showCd">
+              <div class="currentLyric" v-if="currentLyric">
+                <p ref="lyricLine" :class="{'current': currentLineNum === index}" class="text" v-for="(line, index) in currentLyric.lines" :key="index">{{line.txt}}</p>
+              </div>
+              <p class="no-lyric" v-if="currentLyric === null"></p>
+            </div>
+          </scroll>
+        </transition>
+
+        <div class="bottom">
+          <div class="lyric" v-show="toggleLyric"  @click.stop="_hiddenCd">
+            <p class="lyric-desc">{{playingLyric}}</p>
+          </div>
+          <div class="progress-wrapper">
+            <span class="time time-l">{{format(currentTime)}}</span>
+            <div class="progress-bar-wrapper">
+              <progress-bar :percent="percent" @percentChangeEnd="percentChangeEnd"
+                            @percentChange="percentChange"></progress-bar>
+            </div>
+            <span class="time time-r">{{format(duration)}}</span>
+          </div>
+          <div class="control-wrapper">
+            <div class="control">
+              <div class="icon" @click="changeMode">
+                <span class="iconfont" :class="iconMode"></span>
+              </div>
+              <div class="icon" :class="disableCls">
+                <span @click="prev" class="iconfont icon-previous-icon"></span>
+              </div>
+              <div class="icon" :class="disableCls">
+                <span @click="togglePlaying" class="iconfont play-icon" :class="playIcon"></span>
+              </div>
+              <div class="icon" :class="disableCls">
+                <span @click="next" class="iconfont icon-next-icon"></span>
+              </div>
+              <div class="icon">
+                <span class="iconfont icon-dislike"></span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="bottom">
-        <div class="lyric">
-          <p class="lyric-desc">总是学不会，再聪明一点</p>
-        </div>
-        <div class="progress-wrapper">
-          <span class="time time-l">{{format(currentTime)}}</span>
-          <div class="progress-bar-wrapper">
-            <progress-bar :percent="percent" @percentChangeEnd="percentChangeEnd" @percentChange="percentChange"></progress-bar>
-          </div>
-          <span class="time time-r">{{format(duration)}}</span>
-        </div>
-        <div class="control-wrapper">
-          <div class="control">
-            <div class="icon" @click="changeMode">
-              <span class="iconfont" :class="iconMode"></span>
-            </div>
-            <div class="icon" :class="disableCls">
-              <span @click="prev" class="iconfont icon-previous-icon"></span>
-            </div>
-            <div class="icon" :class="disableCls">
-              <span @click="togglePlaying" class="iconfont play-icon" :class="playIcon"></span>
-            </div>
-            <div class="icon" :class="disableCls">
-              <span @click="next" class="iconfont icon-next-icon"></span>
-            </div>
-            <div class="icon">
-              <span class="iconfont icon-dislike"></span>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
     </transition>
     <transition name="mini">
       <div class="mini-player" v-show="!fullScreen" @click="open">
@@ -80,10 +94,11 @@ import ProgressBar from '../../base/progress-bar/progress-bar'
 import {playMode} from '../../common/js/config'
 import {shuffle} from '../../common/js/util'
 import Lyric from 'lyric-parser'
+import Scroll from '../../base/scroll/scroll'
 
 export default {
   name: 'player',
-  components: {ProgressBar},
+  components: {Scroll, ProgressBar},
   data () {
     return {
       url: '',
@@ -93,7 +108,9 @@ export default {
       currentTime: 0,
       duration: 0,
       percent: 0,
-      noLyric: false
+      noLyric: false,
+      toggleLyric: true,
+      playingLyric: ''
     }
   },
   methods: {
@@ -107,6 +124,9 @@ export default {
       const audio = this.$refs.audio
       this.setPlayingState(!this.playing)
       this.playing ? audio.play() : audio.pause()
+      if (this.currentLyric) {
+        this.currentLyric.togglePlay()
+      }
     },
     updateTime (e) {
       this.currentTime = e.target.currentTime
@@ -125,6 +145,9 @@ export default {
       const currentTime = this.duration * percent
       this.currentTime = currentTime
       this.$refs.audio.currentTime = currentTime
+      if (this.currentLyric) {
+        this.currentLyric.seek(currentTime * 1000)
+      }
       if (!this.playing) {
         this.$refs.audio.play()
         this.setPlayingState(true)
@@ -134,6 +157,9 @@ export default {
       this.move = false
       const currentTime = this.duration * percent
       this.$refs.audio.currentTime = currentTime
+      if (this.currentLyric) {
+        this.currentLyric.seek(currentTime * 1000)
+      }
       if (!this.playing) {
         this.$refs.audio.play()
         this.setPlayingState(true)
@@ -167,6 +193,9 @@ export default {
     loop () {
       this.$refs.audio.currentTime = 0
       this.$refs.audio.play()
+      if (this.currentLyric) {
+        this.currentLyric.seek()
+      }
     },
     prev () {
       if (!this.songReady) {
@@ -216,20 +245,39 @@ export default {
       }
       this.noLyric = false
       getLyric(id).then((res) => {
-        console.log(res.data.lrc.lyric)
-      })
-      getLyric(id).then((res) => {
         this.currentLyric = new Lyric(res.data.lrc.lyric, this.handleLyric)
         if (this.playing) {
           this.currentLyric.play()
-          this.currentLineNum = 0
-          this.$refs.lyricList.scrollTo(0, 0, 1000)
+          // this.currentLineNum = 0
+          // this.$refs.lyricList.scrollTo(0, 0, 1000)
         }
       }).catch(() => {
         this.currentLyric = null
         this.noLyric = true
         this.currentLineNum = 0
       })
+    },
+    handleLyric ({lineNum, txt}) {
+      this.currentLineNum = lineNum
+      this.playingLyric = txt
+      if (!this.$refs.lyricList) {
+        return
+      }
+      if (lineNum > 5) {
+        let lineEl = this.$refs.lyricLine[lineNum - 4]
+        this.$refs.lyricList.scrollToElement(lineEl, 1000)
+      } else {
+        if (!this.$refs.lyricList) {
+          return
+        }
+        this.$refs.lyricList.scrollTo(0, 0, 1000)
+      }
+    },
+    _hiddenCd () {
+      this.toggleLyric = false
+    },
+    _showCd () {
+      this.toggleLyric = true
     },
     ...mapMutations({
       setFullScreen: 'SET_FULL_SCREEN',
@@ -311,6 +359,7 @@ export default {
       left 0
       z-index 150
       background #fafafa
+
       .background
         position: absolute
         left: 0
@@ -320,6 +369,7 @@ export default {
         z-index: -1
         opacity: 0.6
         filter: blur(20px)
+
         .mask
           position absolute
           top 0
@@ -328,10 +378,12 @@ export default {
           height 120%
           background rgba(0, 0, 0, .3)
           z-index 0
+
       .top
         display flex
         width 100%
         height 1rem
+
         .under-arrow
           position relative
           z-index 100
@@ -340,6 +392,7 @@ export default {
           color #fff
           line-height 1rem
           margin-left .24rem
+
         .top-title
           flex 1
           line-height 1rem
@@ -347,28 +400,62 @@ export default {
           color #fff
           text-align center
           transform translateX(-.32rem)
+          overflow hidden
+
       .singer
         text-align center
         font-size $font-size-s
         margin-top -.16rem
         color rgba(255, 255, 255, .6)
+
+      .scroll-lyric
+        display inline-block
+        position absolute
+        bottom 4rem
+        top 1.4rem
+        vertical-align top
+        width 100%
+        overflow hidden
+
+        .lyric-wrapper
+          width 80%
+          margin 0 auto
+          overflow hidden
+          text-align center
+          .text
+            line-height .8rem
+            color rgba(255, 255, 255, .4)
+            font-size $font-size-m
+            &.current
+              color #fff
+
+          .no-lyric
+            line-height 40px
+            margin-top 60%
+            color rgba(255, 255, 255, .4)
+            font-size $font-size-m
+
       .album-wrapper
         position relative
+        z-index 100
         width 6rem
         height 6rem
         margin .56rem auto 0 auto
         background url("./album-wrapper.png")
         background-size contain
+
         .img-wrapper
           width 100%
           height 100%
           position absolute
-          z-index -1
           overflow hidden
+
           &.play
             animation rotate 20s linear infinite
+
           &.pause
             animation-play-state paused
+
           .cd-img-wrapper
             width 64%
             height 64%
@@ -377,68 +464,102 @@ export default {
             margin 0 auto
             transform translateY(29%)
             overflow hidden
+
             .cd-img
+              position absolute
+              z-index -100
               display block
               width 100%
+
       .bottom
-        height 100%
+        position fixed
+        width 100%
+        bottom 140px
+
         .lyric
           margin-top 1rem
           width 100%
+
           .lyric-desc
             text-align center
             font-size $font-size-m
             color #fff
+
         .progress-wrapper
           display: flex
           align-items: center
           width: 86%
           margin: .6rem auto 0 auto
           padding: 10px 0
+
           .time
             color #fff
             font-size: $font-size-m
             flex: 0 0 30px
             line-height: 30px
             width: 30px
+
             &.time-l
               text-align: left
               margin-right 10px
+
             &.time-r
               text-align: right
               margin-left 10px
+
           .progress-bar-wrapper
             flex 1
+
         .control-wrapper
           position absolute
           /*bottom 1.5rem*/
           margin-top .3rem
           width 100%
+
           .control
             display flex
             height 1rem
+
             .icon
               flex 1
+
               &.disable
                 opacity .5
+
               span
                 font-size $font-size-xl
                 display block
                 text-align center
                 line-height 1rem
                 color #141a29
+
                 &.play-icon
                   font-size .6rem
+
       &.normal-enter-active, &.normal-leave-active
         transition: all 0.4s
+
         .top, .bottom
           transition: all 0.4s cubic-bezier(0.86, 0.18, 0.82, 1.32)
+
       &.normal-enter, &.normal-leave-to
         opacity: 0
+
         .top
           transform: translate3d(0, -100px, 0)
+
         .bottom
           transform: translate3d(0, 100px, 0)
+
+      .cd-fade-enter, .cd-fade-leave-to
+        opacity 0
+      .cd-fade-enter-active, .cd-fade-leave-to
+        transition all .2s
+
+      .fade-enter, .fade-leave-to
+        opacity 0
+      .fade-enter-active, .fade-leave-to
+        transition all .2s
 
     .mini-player
       display flex
@@ -451,6 +572,7 @@ export default {
       height 1.2rem
       background #fafafa
       box-shadow 0 -4px 14px rgba(112, 115, 123, .1)
+
       .img-wrapper
         position absolute
         left .24rem
@@ -461,46 +583,57 @@ export default {
         background: #000
         overflow hidden
         box-shadow 0 4px 8px rgba(112, 115, 123, .2)
+
         img
           display block
           width 1.2rem
           height 1.2rem
+
           &.play
             animation rotate 20s linear infinite
+
           &.pause
             animation-play-state paused
+
       .song-info
         flex 1
         padding-left 1.8rem
         height 1.2rem
+
         .song-name
           padding-top .2rem
           font-size $font-size-ml
           color $font-color-m
+
         .singer
           padding-top .1rem
           font-size $font-size-s
           color #b3b3b3
+
       .play-button
         width 60px
         height 100%
         text-align center
+
         span
           display block
           height 100%
           line-height 1.2rem
           color $color-theme
           font-size .6rem
+
       .play-list
         width 60px
         height 100%
         text-align center
+
         span
           display block
           height 100%
           line-height 1.2rem
           color $color-theme
           font-size .4rem
+
   @keyframes rotate
     0%
       transform: rotate(0)
